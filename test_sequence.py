9fc2739357686138a894e979bbda94254ae4bd70
SophieZDY <sdzhang@ucdavis.edu>
2025-02-20 15:51:48 -0800

    new

--- test_sequence.py
+++ test_sequence.py
@@ -3,3 +3,7 @@ import sequence
 print(sequence.transcribe('ACGT'))
 print(sequence.revcomp('AAAACGT'))
 print(sequence.translate('ATGCCCTAA')) #should return MX*
+
+s = 'ACGTGGGGGGCATATG'
+print(sequence.gc_comp(s))
+print(sequence.gc_skew(s),sequence.gc_skew(sequence.revcomp(s)))
