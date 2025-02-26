835bd96e105267c70dfebd0a3898ff22ddaff641
SophieZDY <sdzhang@ucdavis.edu>
2025-02-25 17:13:02 -0800

    remove 43skewer.py from repository

--- 43skewer.py
+++
@@ -1,6 +0,0 @@
-import sequence
-seq = 'ACGTACGTGGGGGACGTACGTCCCCC'
-w = 10 #window with size of 10
-for i in range(len(seq) -w +1): #canoical window
-    s = seq[i:i+w]
-    print(i, sequence.gc_comp(s),sequence.gc_skew(s))
